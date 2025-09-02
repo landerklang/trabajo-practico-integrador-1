799d61f027002b270dc53c52f2a1781d818cfd2f
¨lautaro <shortlauty35@gmail.com>
2025-09-02 10:57:36 -0300

    se añadieron las eliminacion logica a usuarios y de cascada a articulo

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -3,16 +3,22 @@ import { sequelize } from "../config/database.js";
 import ProfileModel from "./profile.model.js";
 import ArticleModel from "./article.model.js";
 
-const Usermodel = sequelize.define("UserModel", {
-  username: { type: DataTypes.STRING(20), allowNull: false, unique: true },
-  email: { type: DataTypes.STRING(100), allowNull: false },
-  password: { type: DataTypes.INTEGER(255), allowNull: false },
-  role: {
-    type: DataTypes.ENUM("user", "admin", "guest"),
-    defaultValue: "guest",
-    allowNull: false,
+const Usermodel = sequelize.define(
+  "UserModel",
+  {
+    username: { type: DataTypes.STRING(20), allowNull: false, unique: true },
+    email: { type: DataTypes.STRING(100), allowNull: false },
+    password: { type: DataTypes.INTEGER(255), allowNull: false },
+    role: {
+      type: DataTypes.ENUM("user", "admin", "guest"),
+      defaultValue: "guest",
+      allowNull: false,
+    },
   },
-});
+  {
+    paranoid: true,
+  }
+);
 
 Usermodel.hasOne(ProfileModel, { foreignKey: "user_id", as: "Profile" });
 
@@ -20,6 +26,10 @@ ProfileModel.belongsTo(Usermodel, { foreignKey: "user_id", as: "User" });
 
 Usermodel.hasMany(ArticleModel, { foreignKey: "user_id", as: "Article" });
 
-ArticleModel.belongsTo(Usermodel, { foreignKey: "user_id", as: "User" });
+ArticleModel.belongsTo(Usermodel, {
+  foreignKey: "user_id",
+  as: "User",
+  onDelete: "CASCADE",
+});
 
 export default Usermodel;
