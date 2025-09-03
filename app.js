6f2ba9ea99d2eef24c10e5f6474dc8b3c00cc9de
¨lautaro <shortlauty35@gmail.com>
2025-09-03 12:09:16 -0300

    se añadio el helper para hashear la contraseña

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
-import { sequelize } from "./src/config/database.js";
 import "dotenv/config";
 import morgan from "morgan";
 import { starOn } from "./src/config/database.js";
 import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 
 const app = express();
 const PORT = process.env.PORT;
 
 app.use(express.json());
 app.use(morgan("dev"));
+app.use(
+  cors({
+    origin: "http://localhost:5090",
+    credentials: true,
+  })
+);
+app.use(cookieParser());
 
 app.listen(PORT, async () => {
   await starOn();
